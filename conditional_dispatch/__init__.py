0f9bc9a930cdde5681fe06428290de8f0f300192
Darik A O’Neil <darik.oneil.software@gmail.com>
2024-02-07 19:58:16 -0500

    Create __init__.py

---
+++ conditional_dispatch/__init__.py
@@ -0,0 +1,8 @@
+from .conditional_dispatch import cached_conditional_dispatch, conditional_dispatch
+from .meta import meta
+
+__all__ = [
+  "cached_conditional_dispatch",
+  "conditional_dispatch",
+  "meta",
+]
