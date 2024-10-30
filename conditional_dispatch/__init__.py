3e69c17d4f723d8bbe96992bcd06a75e95a06d63
Darik A O’Neil <darik.oneil.software@gmail.com>
2024-10-30 10:30:34 -0400

    Update __init__.py

--- conditional_dispatch/__init__.py
+++ conditional_dispatch/__init__.py
@@ -1,8 +1,5 @@
-from .conditional_dispatch import cached_conditional_dispatch, conditional_dispatch
-from .meta import meta
+from .conditional_dispatch import conditional_dispatch
 
 __all__ = [
-  "cached_conditional_dispatch",
   "conditional_dispatch",
-  "meta",
 ]
